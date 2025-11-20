32a003c69babb745dd2eb4ac3aff161ec47bd042
toanthanh1 <toanhongngoc0853@gmail.com>
2025-11-20 14:57:57 +0700

    Update LED blinking baremetal

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stm32f4xx.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
